d0ab661256f8f46ce19700995cbe6a0f7d4d2146
肖之胤 <zyxiao95@gmail.com>
2018-10-31 14:17:11 +0800

    读书笔记

--- Effective C++/29_异常安全.cpp
+++ Effective C++/29_异常安全.cpp
@@ -1,12 +1,11 @@
 #include <functional>
 #include <iostream>
 
-namespace ProblemDemo {
+namespace CommonDemo {
 
 class Lock {
   public:
-    explicit Lock(std::mutex *pm) : mutexPtr(pm, [](std::mutex *p){std::cout << "123"; p->unlock();})
-    {
+    explicit Lock(std::mutex *pm) : mutexPtr(pm, [](std::mutex *p) { p->unlock(); }) {
         mutexPtr.get()->lock();
     }
 
@@ -17,15 +16,23 @@ class Lock {
 
 struct Image {
     Image(std::istream &imgSrc) {
-        id = 0;
-    };
-
-    int id;
+        /* Read from istream */
+    }
 };
+}
+
+namespace ProblemDemo {
+using Lock = CommonDemo::Lock;
+using Image = CommonDemo::Image;
 
 class Menu {
     public: 
-        void changeBackground(std:: istream& imgSrc) {
+        Menu() {
+            bgImage = NULL;
+            imageChanges = 0;
+        }
+
+        void changeBackground(std::istream& imgSrc) {
             Lock ml(&mutex);
             delete bgImage;
             ++ imageChanges;
@@ -37,9 +44,81 @@ class Menu {
         Image* bgImage;
         int imageChanges;
 };
+}
 
+namespace BetterDemo {
+using Lock = CommonDemo::Lock;
+using Image = CommonDemo::Image;
+
+/* 
+   这种做法几乎足够提供 changeBackground 强烈的异常安全保证。
+   美中不足是参数imgSrc，如果Image构造函数抛出异常，有可能输入流的读取
+   记号已被移走，而这样的搬移对程序其余部分是一种可见的状态改变。
+ */
+class Menu {
+  public:
+   Menu() { imageChanges = 0; }
+
+   void changeBackground(std::istream &imgSrc) {
+     Lock ml(&mutex);
+     pImpl.reset(new Image(imgSrc));
+     ++imageChanges;
+    }
+
+  private:
+    std::mutex mutex; // 互斥器
+    std::shared_ptr<Image> pImpl;
+    int imageChanges;
+};
+
+/* PrettyMenu Impl
+   封装性已经由 pImpl 是 private 保证了。
+   也可以将 PMImpl 嵌套于PrettyMenu中，但打包问题（packaging，如“独立撰写一场安全码”），
+   是我们这里所挂虑的事。
+ */
+struct PMImpl {
+    std::shared_ptr<Image> bgImage;
+    int imageChanges;
+};
+
+/* 
+   copy and swap 是典型的提供强烈保证的做法
+ */
+class PrettyMenu {
+  public:
+   void changeBackground(std::istream &imgSrc) {
+     using std::swap;
+     Lock ml(&mutex);
+     std::shared_ptr<PMImpl> pNew(new PMImpl(*pImpl));
+     
+     pNew->bgImage.reset(new Image(imgSrc));        // 修改副本
+     ++pNew->imageChanges;
+
+     swap(pNew, pImpl);
+    }
+
+  private:
+    std::mutex mutex; // 互斥器
+    std::shared_ptr<PMImpl> pImpl;
+};
 }
 
 int main() {
+    using PMenu = ProblemDemo::Menu;
+    PMenu *pm = new PMenu();
+    pm->changeBackground(std::cin);
+
     return 0;
 }
+
+/* 
+   要点：
+   异常安全性的函数满足条件：
+    不泄漏任何资源
+    不允许数据败坏
+   异常安全性的函数会提供以下保证：
+    基本承诺：
+    强烈保证：
+    不抛掷保证：
+   
+ */
